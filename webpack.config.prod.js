f3c2b66dda862421445bbbe30185a6b6c3d23410
David Kindler <david.kindler@nxp.com>
2020-01-30 09:53:02 -0600

    update dev settings

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,44 +1,45 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-// process.traceDeprecation = true;
-// https://github.com/webpack/loader-utils/issues/56
 
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: __dirname+'/dist',
+    path: __dirname + '/dist',
     filename: 'index_bundle.js'
   },
-  plugins:[
-    new webpack.DefinePlugin({ 
+  optimization: {
+    minimize: true
+  },
+  plugins: [
+    new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new webpack.optimize.UglifyJsPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
-  ],
-  // Uncomment for sourcemaps.  This increase size of file greatly.
-  // devtool: 'eval-source-map',
+    })],
   module: {
-        rules : [
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
           {
-            loader: 'babel-loader',
-            query:{ presets: ['react','es2015']},
-            test: /\.jsx?$/,
-            exclude: /(node_modules)/
+            loader: "html-loader"
           }
         ]
-  },
-  resolve: {
-    extensions: [' ', '.js', '.jsx', '.json']
-  },
-    // Uncomment if you do not want to include react and react-dom in bundle.js  
-    // },                                                                        
-         
+      }
+    ]
+  }
+
 };
